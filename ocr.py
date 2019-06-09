a3350af10a4e45ec858f8363b9614439bafadf16
Caleb Carr <Caleb.M.Carr-1@ou.edu>
2019-06-09 17:00:09 -0500

    Read Latin text with OCR
    Replacement of older ocr.py

--- ocr.py
+++ ocr.py
@@ -1,17 +1,20 @@
 '''Scans through every file in /dev/text_seg to read Latin text.
 Each page is segmented by part, saved to text_seg then deleted for the 
-next page.
+next page. Result of each image is sent to /dev/ocr_seg to be read by 
+the XML formatter.
 '''
 try:
 	from PIL import Image
 except ImportError:
 	import Image
 import pytesseract
+import os
 from os import listdir
 from os.path import isfile, join
 
 #set directory
 dir = './dev/text_seg/'
+dump_dir = './dev/ocr_seg/'
 #get all the files in segmented text 
 photos = [f for f in listdir(dir) if isfile(join(dir,f))]
 print(photos)
@@ -19,5 +22,8 @@ text = []
 def ocr_read(filename):
 	for photo in range(len(photos)):
 		text.append(pytesseract.image_to_string(Image.open(dir+photos[photo])))
+		f = open(dump_dir+os.path.splitext(photos[photo])[0]+'.txt','w+')
+		f.write(text[photo])
+		f.close()
 	return text
 print(ocr_read(dir))
